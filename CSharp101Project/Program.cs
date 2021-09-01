b11005ae54acd423abcbad983fd441df4b20b738
willi <willi@DESKTOP-L87Q9QN>
2021-09-01 21:16:01 +0300

    done

--- CSharp101Project/Program.cs
+++ CSharp101Project/Program.cs
@@ -10,8 +10,38 @@ namespace CSharp101Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Yes");
-            Console.WriteLine("No");
+            var studentsNum = 0;
+            var StudentName = new List<string>();
+            var grade = new List<double>();
+
+
+            Console.WriteLine("How many students do you want to enter their grades?");
+            studentsNum = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < studentsNum; i++)
+            {
+                Console.WriteLine("Enter the student's name No: "+ (i+1));
+                StudentName.Add(Console.ReadLine());
+                Console.WriteLine("Enter the student's ( "+ StudentName[i] + " ) grade: ");
+                grade.Add(Convert.ToDouble(Console.ReadLine()));
+
+            }
+            Console.Write("No \t");
+            Console.Write("Name \t");
+            Console.Write("Grade \t");
+            Console.WriteLine("Status");
+
+            for (int i = 0; i < StudentName.Count; i++)
+            {
+                Console.Write((i + 1) + "\t");
+                Console.Write(StudentName[i] + "\t");
+                Console.Write(grade[i] + "\t");
+                if (grade[i] >= 60)
+                    Console.WriteLine("Passed");
+                else
+                    Console.WriteLine("Failed");
+
+            }
+
             Console.ReadKey();
 
         }
